7e2625960d65930220dc3f2f36b6e4d5c5203070
Anton Tananaev <anton.tananaev@gmail.com>
2016-07-02 01:16:02 +1200

    Add unit test to the project

---
+++ src/test/java/com/tananaev/adblib/AdbConnectionTest.java
@@ -0,0 +1,30 @@
+package com.tananaev.adblib;
+
+import org.junit.Test;
+
+import javax.xml.bind.DatatypeConverter;
+import java.net.Socket;
+
+public class AdbConnectionTest {
+
+    @Test
+    public void testConnection() throws Exception {
+
+        Socket socket = new Socket("192.168.1.15", 5555);
+
+        AdbCrypto crypto = AdbCrypto.generateAdbKeyPair(new AdbBase64() {
+            @Override
+            public String encodeToString(byte[] data) {
+                return DatatypeConverter.printBase64Binary(data);
+            }
+        });
+
+        AdbConnection connection = AdbConnection.create(socket, crypto);
+
+        connection.connect();
+
+        AdbStream stream = connection.open("shell");
+
+    }
+
+}
